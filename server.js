a809551eb500b45a9c4148c4d8492da5d45c0353
manojitballav <manojitocto@gmail.com>
2019-12-25 12:47:37 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -15,7 +15,17 @@ app.get('/users',function(req,res){
 	})
 })
 
-app.listen(8000,function(){console.log('server is alert')})
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+app.listen(8000,function(){console.log('Server is Alert')})
+
 // app.get('/', function(req, res) {
 //         console.log("server is being whipped")
 //       });
