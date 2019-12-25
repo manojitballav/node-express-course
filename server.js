ee6a784b85da542bdcb5e46ac08449cce292e6c0
manojitballav <manojitocto@gmail.com>
2019-12-25 12:17:10 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-app.listen(8000,function(){
-    console.log("server is being whipped")
-    })
+
+// Adding mock user data
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
+app.listen(8000,function(){console.log('server is alert')})
 // app.get('/', function(req, res) {
 //         console.log("server is being whipped")
 //       });
